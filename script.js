f2ce851dfc9fef674947fffb5beb29d78c0c7ec1
Kristine Baillieul <kbaillieul1@gmail.com>
2021-10-12 17:04:37 -0230

    Add plant images to canvas based on dropdown option selected

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ const ctx = canvas.getContext("2d");
 let plantSubmit = document.getElementById("plantButton");
 plantSubmit.addEventListener("click", addPlant);
 
+const tomato = new Image();
+tomato.src = "images/tomato1.png";
 const corn = new Image();
 corn.src = "images/corn1.png";
-corn.onload = function () {
-  ctx.drawImage(corn, 100, 350);
-};
+const sunflower = new Image();
+sunflower.src = "images/sunflower1.png";
 
 function addPlant() {
   let input = document.getElementById("plantType").value;
-  console.log(input);
+  if (input === "tomato") {
+    ctx.drawImage(tomato, 100, 275, 150, 150);
+  } else if (input === "corn") {
+    ctx.drawImage(corn, 300, 275, 150, 150);
+  } else if (input === "sunflower") {
+    ctx.drawImage(sunflower, 500, 275, 150, 150);
+  }
 }
